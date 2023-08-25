29279a2f1eb1a2cbe4f497ec3ed867664574fb37
Kimbill <ayooluwaa@decagonhq.com>
2023-08-25 15:17:36 +0100

    modification

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace ConsoleApp12
     class BankAccount
     {
 
-        private (int, bool, str) balance;
+        private int balance;
 
         public void Deposit(int amount)
         {
@@ -41,5 +41,45 @@ namespace ConsoleApp12
             bankAccount.Withdraw(200);
             Console.WriteLine($"Balance: {bankAccount.GetBal()}");
         }
+
+        public class Account
+        {
+            private int balance;
+            private bool depositSuccess;
+            private string depositMessage;
+
+            public Account()
+            {
+                balance = 0;
+                depositSuccess = false;
+                depositMessage = string.Empty;
+            }
+
+            public void Deposit(int amount)
+            {
+                if (amount > 0)
+                {
+                    balance += amount;
+                    depositSuccess = true;
+                    depositMessage = "Deposit successful";
+                }
+                else
+                {
+                    depositSuccess = false;
+                    depositMessage = "Deposit amount must be positive";
+                }
+            }
+
+            //public (int Balance, bool DepositSuccess, string DepositMessage) GetBalanceAndDepositStatus()
+            //{
+            //    return (balance, depositSuccess, depositMessage);
+            //}
+
+            public (string DepositMessage, int Balance, bool DepositSuccess) GetBalanceDeposit()
+            {
+                return (balance, depositMessage, depositSuccess);
+            }
+        }
+
     }
 }
