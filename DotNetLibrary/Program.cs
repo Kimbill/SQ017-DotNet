9f2746939253f23062d5f6807ac68ca1268c8f91
Kimbill <ayooluwaa@decagonhq.com>
2023-08-31 08:00:09 +0100

    added repository pattern example

---
+++ DotNetLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using DotnetLibrary.Data.Repositories.implementations;
+using DotnetLibrary.Data.Repositories.Interfaces;
+using System;
+
+namespace DotNetLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBookRepository bookRepository = new BookRepository();
+
+            bookRepository.Add(new DotnetLibrary.Model.Book { Id = 1, Title = "OOP 1", Author = "Chinedu" });
+
+            var firstBook = bookRepository.GetById(1);
+            Console.WriteLine($"OOP 1: {firstBook.Title} by {firstBook.Author}");
+        }
+    }
+}
